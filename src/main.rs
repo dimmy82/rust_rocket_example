eac1301baa7d3ec7661afc857a2520f93ae8102c
teki.sei <teki.sei@uzabase.com>
2019-05-26 20:35:16 +0900

    rocket init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+
+use diesel::mysql::MysqlConnection;
+use reqwest::Client;
+
+#[database("db_mysql")]
+pub struct DbMysqlConnection(MysqlConnection);
+
+#[derive(Clone)]
+pub struct HttpClient {
+    pub reqwest_client: Client,
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![yao]).launch();
+}
+
+#[get("/yao")]
+fn yao() -> &'static str {
+    "ming"
 }
